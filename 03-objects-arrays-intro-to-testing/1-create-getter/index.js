50ebaaa024992ba522ca0a41dd5975ab4b8187f6
AKurbatov <AKurbatov83@mail.ru>
2021-05-10 00:01:00 +1100

    Урок 3. Задача 1
    Реализована функция "createGetter", которая возвращает новую функцию,
    которая по заданному пути находит значение в переданном ей объекте и
    возвращает это значение.

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,13 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+    let pathItems = path.split(".");
+    
+    return function(obj) {
+        for (let item of pathItems) {
+            if (!obj[item]) return;
+            obj = obj[item];
+        }
+        return obj;
+    }
 }
